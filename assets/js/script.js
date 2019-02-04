0ed9ccb56b54b576e2e7ec469f8db49072b999f5
Josh Baker <jbaker19982017@outlook.com>
2019-02-04 15:40:36 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#button_effects1").click(function() {
+        $('#par1').slideToggle('slow');
+    });
+    $("#button_effects2").click(function() {
+        $('#par2').slideToggle('slow');
+    });
+    $("#button_effects3").click(function() {
+        $('#par3').slideToggle('slow')
+    });
+});
